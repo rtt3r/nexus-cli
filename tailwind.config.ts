46ec4744de579342c5e61691d85cd8f262fa4c4e
Anderson Rtitter de Souza <anderdsouza@gmail.com>
2024-11-02 09:45:24 -0300

    Ajustes tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+const config: Config = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          'Nunito',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+    },
+  }
+};
+
+export default config;
